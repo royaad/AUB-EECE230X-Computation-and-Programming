961fee6260846d91f5b173f9fd1a083f1f37908f
royaad <royaad@live.com>
2023-11-25 18:08:24 +0200

    Final exam

---
+++ Final Exam/Problem3.py
@@ -0,0 +1,11 @@
+# Roy Aad
+
+def pattern(k):
+    if k <= 1:
+        return "1"
+    else:
+        
+        return pattern(k - 1) + "0" * k + pattern(k-2)
+    
+for k in range(0,6):
+    print("pattern("+str(k)+"): " + pattern(k))
