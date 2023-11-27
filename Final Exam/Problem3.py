ddcdf2cf560265f3cacd4ef76ea6ab43035bea7a
royaad <royaad@live.com>
2023-11-27 09:07:26 +0200

    Problem3 Memoized

--- Final Exam/Problem3.py
+++ Final Exam/Problem3.py
@@ -1,10 +1,13 @@
 # Roy Aad
 
-def pattern(k):
+def pattern(k, mem={}):
+    if k in mem: return mem[k]
     if k <= 1:
+        mem[k] = "1"
         return "1"
     else:
-        return pattern(k - 1) + "0" * k + pattern(k-2)
+        mem [k] = pattern(k - 1, mem) + "0" * k + pattern(k-2, mem)
+        return mem[k]
     
 for k in range(0,6):
     print("pattern("+str(k)+"): " + pattern(k))
